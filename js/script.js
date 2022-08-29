18208460f1b8b327efd03a2e43eb1886610a7c28
Benshu Xu <benshuxu@gmail.com>
2022-08-29 17:07:28 +1000

    Code Test JS

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
 // write your code here
+function checkThree(num) {
+  if (num % 3 === 0) {
+    return true;
+  } else return false;
+}
 
+function checkFive(num) {
+  if (num % 5 === 0) {
+    return true;
+  } else return false;
+}
 
+for (let i = 1; i <= 100; i++) {
+  if (checkFive(i) && checkThree(i)) {
+    console.log("FizzBuzz");
+    continue;
+  }
+  if (checkThree(i)) console.log("Fizz");
+  else if (checkFive(i)) console.log("Buzz");
+  else console.log(i);
+}
